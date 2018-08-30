e622f3e47259cd3aaf4ec80aed1467c054b9285a
Harry Bloch <harryfbloch@gmail.com>
2018-08-30 16:29:58 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -13,7 +13,7 @@ function frameIt(){
 
 function pressIt(){
   $("form").on("keydown" , function(key){
-    if (key.keyCode == 71){
+    if (key.keyCode === 71){
       alert("You Pressed G");
     }
   })
